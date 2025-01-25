c1999ae46b41389abe0447c78a24d165924eeeea
evan-blough <167023108+evan-blough@users.noreply.github.com>
2025-01-25 17:52:26 -0500

    better

---
+++ src/BasketballEventHandler.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Timers;
+
+namespace basketball_app
+{
+    public class ScoreHandler
+    {
+        public int HomeTeamScore = 0;
+        public int AwayTeamScore = 0;
+        StreamWriter sw;
+
+
+        public string UpdateScore(bool isHome, int toAdd)
+        {
+            if (isHome)
+                HomeTeamScore += toAdd;
+            else
+                AwayTeamScore += toAdd;
+
+            WriteScore(isHome);
+
+            return isHome ? HomeTeamScore.ToString() : AwayTeamScore.ToString();
+        }
+
+        public void ResetScore()
+        {
+            HomeTeamScore = 0;
+            AwayTeamScore = 0;
+
+            WriteScore(true);
+            WriteScore(false);
+        }
+
+        public void WriteScore(bool isHome)
+        {
+            if (isHome)
+            {
+                sw = new StreamWriter("homeScore.txt");
+                sw.Write(HomeTeamScore);
+                sw.Close();
+            }
+            else
+            {
+                sw = new StreamWriter("awayScore.txt");
+                sw.Write(AwayTeamScore);
+                sw.Close();
+            }
+        }
+    }
+
+    public class FoulHandler
+    {
+        public int HomeTeamFouls = 0;
+        public int AwayTeamFouls = 0;
+        StreamWriter sw;
+
+
+        public string AddFouls(bool isHome)
+        {
+            if (isHome)
+                HomeTeamFouls += 1;
+            else
+                AwayTeamFouls += 1;
+
+            WriteFouls(isHome);
+
+            return isHome ? HomeTeamFouls.ToString() : AwayTeamFouls.ToString();
+        }
+
+        public string RemoveFouls(bool isHome)
+        {
+            if (isHome)
+                HomeTeamFouls = HomeTeamFouls == 0 ? 0 : HomeTeamFouls - 1;
+            else
+                AwayTeamFouls = AwayTeamFouls == 0 ? 0 : AwayTeamFouls - 1;
+
+            WriteFouls(isHome);
+
+            return isHome ? HomeTeamFouls.ToString() : AwayTeamFouls.ToString();
+        }
+
+        public void ResetFouls()
+        {
+            HomeTeamFouls = 0;
+            AwayTeamFouls = 0;
+
+            WriteFouls(true);
+            WriteFouls(false);
+        }
+
+        public void WriteFouls(bool isHome)
+        {
+            if (isHome)
+            {
+                sw = new StreamWriter("homeFouls.txt");
+                sw.Write(HomeTeamFouls);
+                sw.Close();
+            }
+            else
+            {
+                sw = new StreamWriter("awayFouls.txt");
+                sw.Write(AwayTeamFouls);
+                sw.Close();
+            }
+        }
+    }
+
+    public class TimeoutHandler
+    {
+        public int HomeTeamTimeouts = 5;
+        public int AwayTeamTimeouts = 5;
+        StreamWriter sw;
+
+        public string AddTimeout(bool isHome)
+        {
+            Debug.WriteLine($"Home: {HomeTeamTimeouts}");
+            Debug.WriteLine($"Away: {AwayTeamTimeouts}");
+            if (isHome)
+                HomeTeamTimeouts += 1;
+            else
+                AwayTeamTimeouts += 1;
+
+            WriteTimeouts(isHome);
+
+            return isHome ? HomeTeamTimeouts.ToString() : AwayTeamTimeouts.ToString();
+        }
+
+        public string RemoveTimeout(bool isHome)
+        {
+            Debug.WriteLine($"Home: {HomeTeamTimeouts}");
+            Debug.WriteLine($"Away: {AwayTeamTimeouts}");
+            if (isHome)
+                HomeTeamTimeouts = HomeTeamTimeouts == 0 ? 0 : HomeTeamTimeouts - 1;
+            else
+                AwayTeamTimeouts = AwayTeamTimeouts == 0 ? 0 : AwayTeamTimeouts - 1;
+
+            WriteTimeouts(isHome);
+
+            return isHome ? HomeTeamTimeouts.ToString() : AwayTeamTimeouts.ToString();
+        }
+
+        public void ResetTimeouts()
+        {
+            Debug.WriteLine($"Home: {HomeTeamTimeouts}");
+            Debug.WriteLine($"Away: {AwayTeamTimeouts}");
+            HomeTeamTimeouts = 5;
+            AwayTeamTimeouts = 5;
+            WriteTimeouts(true);
+            WriteTimeouts(false);
+        }
+
+        public void WriteTimeouts(bool isHome)
+        {
+            if (isHome)
+            {
+                sw = new StreamWriter("homeTimeouts.txt");
+                sw.Write(HomeTeamTimeouts);
+                sw.Close();
+            }
+            else
+            {
+                sw = new StreamWriter("awayTimeouts.txt");
+                sw.Write(AwayTeamTimeouts);
+                sw.Close();
+            }
+        }
+    }
+
+    public class TimeHandler
+    {
+        public Timer timer = new Timer();
+        Action<string> changeTime;
+        public int quarterTime = 4800;
+        public int halfTime = 6000;
+        public int time = 4800;
+        public int quarter = 1;
+        StreamWriter sw;
+
+        public void InitializeTimer(Action<string> action)
+        {
+            // measured in tenths of seconds
+            timer.Interval = 93.75;
+            timer.Elapsed += OnTimerTick;
+            timer.Enabled = true;
+            timer.AutoReset = true;
+            timer.Stop();
+            changeTime = action;
+
+            FindTime();
+        }
+
+        private void OnTimerTick(Object source, EventArgs e)
+        {
+            if (time > 0)
+            {
+                time -= 1;
+
+                var currTime = FindTime();
+                changeTime(currTime);
+            }
+            else
+            {
+                timer.Stop();
+                WriteTime("0.0");
+            }
+        }
+
+        public string SetHalftime()
+        {
+            time = halfTime;
+            quarter += 1;
+            return FindTime();
+        }
+        public string ResetClock()
+        {
+            quarter = 1;
+            time = quarterTime;
+            return FindTime();
+        }
+
+        public string ResetTime()
+        {
+            time = quarterTime;
+            return FindTime();
+        }
+        public string AddOneSecond()
+        {
+            time += 10;
+            return FindTime();
+        }
+        public string SubtractOneSecond()
+        {
+            time = time < 10 ? 0 : time - 10;
+            return FindTime();
+        }
+        public string AddOneMinute()
+        {
+            time += 600;
+            return FindTime();
+        }
+
+        public string SubtractOneMinute()
+        {
+            time = time < 600 ? 0 : time - 600;
+            return FindTime();
+        }
+
+        public string FindTime()
+        {
+            int tempTime = time;
+            int minutes = time / 600;
+
+            tempTime -= minutes * 600;
+
+            string seconds = (tempTime / 10).ToString("D2");
+
+            string newTime;
+            if (time / 600 > 0)
+                newTime = $"{minutes}:{seconds}";
+            else
+                newTime = $"{seconds}.{time % 10}";
+
+            WriteTime(newTime);
+            return newTime;
+        }
+
+        public void WriteTime(string newTime)
+        {
+            sw = new StreamWriter("timer.txt");
+            sw.Write(newTime);
+            sw.Close();
+        }
+    }
+}
---
+++ src/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace basketball_app
+{
+    public partial class Scoreboard : Form
+    {
+        private string currentTime;
+
+        public ScoreHandler score = new ScoreHandler();
+        public FoulHandler fouls = new FoulHandler();
+        public TimeoutHandler timeouts = new TimeoutHandler();
+        public TimeHandler time = new TimeHandler();
+
+        public Scoreboard()
+        {
+            InitializeComponent();
+            SetupText();
+            time.InitializeTimer(ChangeTime);
+        }
+        public void SetupText()
+        {
+            label1.Text = score.HomeTeamScore.ToString();
+            label2.Text = score.AwayTeamScore.ToString();
+            label7.Text = fouls.HomeTeamFouls.ToString();
+            label8.Text = fouls.AwayTeamFouls.ToString();
+            label11.Text = timeouts.HomeTeamTimeouts.ToString();
+            label12.Text = timeouts.AwayTeamTimeouts.ToString();
+            label18.Text = time.FindTime();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = score.UpdateScore(true, 1).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = score.UpdateScore(true, 2).ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = score.UpdateScore(true, 3).ToString();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = score.UpdateScore(false, 1).ToString();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            label2.Text = score.UpdateScore(false, 2).ToString();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label2.Text = score.UpdateScore(false, 3).ToString();
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label1.Text = score.UpdateScore(true, -1).ToString();
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            label2.Text = score.UpdateScore(false, -1).ToString();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            score.ResetScore();
+            label1.Text = score.HomeTeamScore.ToString();
+            label2.Text = score.AwayTeamScore.ToString();
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            label7.Text = fouls.AddFouls(true);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label8.Text = fouls.AddFouls(false);
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            fouls.ResetFouls();
+            label7.Text = fouls.HomeTeamFouls.ToString();
+            label8.Text = fouls.AwayTeamFouls.ToString();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            label7.Text = fouls.RemoveFouls(true);
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            label8.Text = fouls.RemoveFouls(false);
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            label11.Text = timeouts.AddTimeout(true);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            label12.Text = timeouts.AddTimeout(false);
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            label11.Text = timeouts.RemoveTimeout(true);
+        }
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            label12.Text = timeouts.RemoveTimeout(false);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            timeouts.ResetTimeouts();
+            label11.Text = timeouts.HomeTeamTimeouts.ToString();
+            label12.Text = timeouts.AwayTeamTimeouts.ToString();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            time.timer.Start();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            time.timer.Stop();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            time.timer.Stop();
+            time.time = time.quarterTime;
+            time.quarter = time.quarter >= 4 ? 1 : time.quarter += 1;
+            label18.Text = time.FindTime();
+            label15.Text = time.quarter.ToString();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            time.timer.Stop();
+            label18.Text = time.quarter == 2 ? time.SetHalftime() : (time.quarter == 4 ? time.ResetClock() : time.FindTime());
+
+            label15.Text = time.quarter.ToString();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            time.timer.Stop();
+            label18.Text = time.ResetTime();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            label18.Text = time.AddOneMinute();
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            label18.Text = time.SubtractOneMinute();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            label18.Text = time.AddOneSecond();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            label18.Text = time.SubtractOneSecond();
+        }
+
+        public void ChangeTime(string currTime)
+        {
+            currentTime = currTime;
+
+            if (label18.InvokeRequired)
+            {
+                label18.BeginInvoke((MethodInvoker)delegate ()
+                {
+                    if (currentTime != label18.Text)
+                        label18.Text = currentTime;
+                });
+            }
+            else
+            {
+                if (currentTime != label18.Text)
+                    label18.Text = currentTime;
+            }
+        }
+
+    }
+}
